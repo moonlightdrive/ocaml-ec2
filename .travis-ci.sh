72ae89f17532005255b42c67491c0530447035d2
Jyotsna Prakash <jyotsna.prakash@gmail.com>
2014-06-15 15:33:48 -0700

    typo in .travis-ci.sh

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,6 +1,13 @@
-OPAM_DEPENDS="lwt cohttp ezxmlm uri cryptokit core_extended threads"
+export OPAM_DEPENDS="lwt cohttp ezxmlm uri cryptokit core_extended threads"
 
-ppa=avsm/ocaml41+opam11
+case "$OCAML_VERSION,$OPAM_VERSION" in
+4.00.1,1.0.0) ppa=avsm/ocaml40+opam10 ;;
+4.00.1,1.1.0) ppa=avsm/ocaml40+opam11 ;;
+4.01.0,1.0.0) ppa=avsm/ocaml41+opam10 ;;
+4.01.0,1.1.0) ppa=avsm/ocaml41+opam11 ;;
+*) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1 ;;
+esac
+
 echo "yes" | sudo add-apt-repository ppa:$ppa
 sudo apt-get update -qq
 sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
