14128923375a65a11079367e3d16cf3406ba8ea3
Jyotsna Prakash <jyotsna.prakash@gmail.com>
2014-06-28 10:21:56 -0700

    update describe_regions test

--- lib_test/describe_regions.ml
+++ lib_test/describe_regions.ml
@@ -3,5 +3,7 @@ open EC2_t
 
 let _ = 
   let describe_regs = Lwt_main.run (Monad.run (Regions.describe () )) in
-  let print_resp {name; endpoint} = print_endline (Printf.sprintf "%s: %s" name endpoint) in 
+  let print_resp {name; endpoint} = print_endline (Printf.sprintf "%s: %s" 
+								  (string_of_region name) 
+								  endpoint) in 
   List.map print_resp describe_regs
