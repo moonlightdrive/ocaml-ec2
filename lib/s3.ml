be7f7b3234f99a1149b81c7f3ba1deff2810127e
Jyotsna Prakash <jyotsna.prakash@gmail.com>
2014-08-08 15:00:43 -0700

    s3: upload BIN files properly

--- lib/s3.ml
+++ lib/s3.ml
@@ -21,21 +21,28 @@ let put ?region bucket obj =
 			   (* Yes this is totally weird but 
                               see https://github.com/mirage/ocaml-uri/issues/51 *)
 			   Uri.to_string |> Uri.of_string in
-  let obj_contents_body = Cohttp_lwt_body.of_stream @@ Lwt_io.lines_of_file obj in
-  lwt obj_contents_str = Cohttp_lwt_body.to_string @@ obj_contents_body in
-    lwt length = Cohttp_lwt_body.length obj_contents_body in
-    print_int @@ fst length;
-    
+  let (body, stream) = 
+    let open Cohttp_lwt_body in
+    let contents f = of_stream @@ Lwt_io.lines_of_file f in
+    (contents obj, contents obj) in
+  lwt body_str = Cohttp_lwt_body.to_string stream in
+    lwt body = 
+      let open Lwt_io in
+      let open Lwt in
+      lwt len = Lwt_io.file_length obj >|= Int64.to_int in
+    let buffer = String.create len in
+    Lwt_io.with_file ~buffer_size:len ~mode:input obj 
+		     (fun ic -> Lwt_io.read_into_exactly ic buffer 0 len)
+    >>= fun () -> return buffer in
     let headers = 
       (* TODO region *)
       let region = match region with
-	| Some r -> string_of_region r
+	| Some r -> EC2_t.string_of_region r
 	| None -> match Sys.getenv "REGION" with exn -> EC2_t.(string_of_region US_EAST_1) in
-      realize_headers meth uri obj_contents_str s3 region ~acl:"authenticated-read" in 
-    let headers = Cohttp.Header.add headers "x-amz-content-sha256" (Hash.hex_hash obj_contents_str) in
+      realize_headers meth uri body s3 region ~acl:"authenticated-read" in 
+    let headers = Cohttp.Header.add headers "x-amz-content-sha256" (Hash.hex_hash body) in
     request Monad.({ api = s3;
-		     (* TODO just did a Cohttp_lwt_body.to_string... use obj-contents_body? *)
-		     body = Cohttp_lwt_body.of_string obj_contents_str;
+		     body = Cohttp_lwt_body.of_string body;
 		     headers = headers;
 		     meth = meth;
 		     uri = uri; })
