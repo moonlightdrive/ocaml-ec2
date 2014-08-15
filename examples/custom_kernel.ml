10f03ae43ca2c12223302c93ff010a2723366abf
Jyotsna Prakash <jyotsna.prakash@gmail.com>
2014-08-15 00:09:29 -0700

    add examples

--- examples/custom_kernel.ml
+++ examples/custom_kernel.ml
@@ -8,19 +8,20 @@ let my_cert = "mycert.pem"
 let region = US_WEST_2
 let my_bucket = "mybucket"
 
-let start_instance img_file key cert bucket =
+let start_instance img_file key cert bucket () =
   print_endline "Bundling image...";
   let (manifest_path, part_paths) as files = Bundle.bundle_img ~key ~cert img_file |> Lwt_main.run in
   print_endline "Uploading bundle (this may take a while)...";
   ignore @@ List.map Lwt_main.run @@ Bundle.upload ~bucket files ~region;
   print_endline "Registering AMI...";
   let img_path = 
-    let kernel =  manifest_path (*Filename.basename manifest_path*) in
+    let kernel =  manifest_path in
     Printf.sprintf "%s%s" bucket kernel in
   Monad.bind (AMI.register_image ~name:"ocaml-ec2" ~img_path ~region ())
 	     (fun id -> print_endline @@ 
 			  Printf.sprintf "Launching instance %s" (ImageID.to_string id);
 			Instances.run id ~region ()) 
-	     
-let _ = Lwt_main.run  
-	  (Monad.run @@ start_instance image my_key my_cert my_bucket)
+
+let _ = Lwt_main.run @@ 
+	  Monad.run @@ start_instance image my_key my_cert my_bucket ()
+
